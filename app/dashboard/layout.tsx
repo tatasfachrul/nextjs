73f74b397be9892fde503d7eef82b3af1fbd5cd2
Tatas Fachrul <tatasfachrul@gmail.com>
2025-09-10 15:05:56 +0700

    initialization

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-1 md:overflow-auto">{children}</div>
+    </div>
+  );
+}
